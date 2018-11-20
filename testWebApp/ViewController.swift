4d9f72804bf7be0a1ce26d071e1ed6a495d85ce5
Bohddan Paliychuk <bpaliychuk@bpaliychuk-mbp.malkosua.com>
2018-11-20 13:03:42 +0200

    Initial Commit

---
+++ testWebApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testWebApp
+//
+//  Created by Bohdan Paliychuk on 11/20/18.
+//  Copyright © 2018 Bohdan Paliychuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
